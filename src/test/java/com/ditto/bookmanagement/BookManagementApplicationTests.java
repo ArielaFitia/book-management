fbf15965c55accdb13dc1b1f88c1e45953ef6d0c
ArielaFitia <arielafitiajoharini@gmail.com>
2025-05-12 15:45:27 +0300

    Initial commit

---
+++ src/test/java/com/ditto/bookmanagement/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ditto.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
