8822a749428e3c9cf4574148e5b8e8194688f36c
ValisoaMbolatiana <fanomezantsoavalisoa@gmail.com>
2025-05-13 23:02:59 +0300

    add controller

--- src/main/java/com/ditto/bookmanagement/controller/BookController.java
+++ src/main/java/com/ditto/bookmanagement/controller/BookController.java
@@ -1,15 +1,47 @@
 package com.ditto.bookmanagement.controller;
 
+
+
+import com.ditto.bookmanagement.dto.BookDTO;
 import com.ditto.bookmanagement.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
-    @Autowired
-    private BookService bookService;
+    private final BookService bookService;
 
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDTO createBook(@RequestBody BookDTO bookDTO) {
+        return bookService.addBook(bookDTO);
+    }
+
+    @GetMapping
+    public List<BookDTO> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PutMapping("/{id}")
+    public BookDTO updateBook(@PathVariable Long id, @RequestBody BookDTO bookDTO) {
+        return bookService.updateBook(id, bookDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
 }
